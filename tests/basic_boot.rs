2f7183379e8a0f49e52ea9982f186c7f620d6d3b
Dale Morris <dalemorris2021@gmail.com>
2026-03-21 08:08:33 +0000

    Implemented testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,28 +7,19 @@
 use core::panic::PanicInfo;
 use tellus::println;
 
-/// this function is the entry point
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello, World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// this function is called on panic
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-/// this function is called on panic
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     tellus::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
